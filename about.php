306fc806efc2e6032268ccb28fe4b261767c7ea8
NgocSrp <leminhngoc17111999@gmail.com>
2022-06-05 19:10:17 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,2 @@
+<h1><?php echo $title;?></h1>
+<p>Sinh viên CNTT, Trường Đại học Vinh </p>
---
+++ home.php
@@ -0,0 +1,33 @@
+<!-- <h1><?php echo $title;?></h1> -->
+<div style="font-size: 20px;">  
+<h2>Giới thiệu chung về Website</h2>
+    <p>Website chủ yếu cung cấp hướng dẫn cho các bạn những bài học, bài tập và những tài liệu để các bạn có thể học tốt tiếng anh.
+    </p> 
+</div>
+<div style="font-size: 20px;" >
+  <h2>Mục tiêu</h2> 
+   <ol>
+      <li>Tạo môi trường chia sẻ trách nhiệm và lãnh đạo với quản trị viên có năng lực.</li>
+      <li>Cung cấp các khóa đào tạo và hội thảo có liên quan cho giảng viên, nhân viên và sinh viên.</li>
+      <li>Tạo ra những sinh viên tốt nghiệp có tính cạnh tranh cao.</li>
+      <li>Trở thành mội người thành tạo về ngoại ngữ</li>
+   </ol> 
+</div>
+<div  style="font-size: 20px;">
+  <h2>Nguyên tắc hướng dẫn</h2> 
+    <ul>
+      <li>Tự do học tập </li>
+      <li>Nhiệm vụ</li>
+      <li>Tiêu chuẩn học tập</li>  
+    </ul>
+<div style="font-size: 20px;">
+  <h2>Chính sách chất lượng</h2> 
+  <ol>
+      <li>Website cam kết trở thành trung tâm xuất sắc trong học tập cung cấp dịch vụ chất lượng cho tất cả mọi người.</li> 
+      <li>Tiếp tục đổi mới và cải tiến chất lượng nhằm xây dựng một môi trường hiệu quả và hiệu quả để đạt được sự hài lòng tối đa của khách hàng.</li> 
+      <li>Trưng bày đầu ra chất lượng.</li> 
+      <li>Duy trì các giá trị và sự chính trực, đồng thời nuôi dưỡng các tài năng và kỹ năng cho khả năng cạnh tranh toàn cầu.</li>  
+  </ol>
+</div>
+ 
+
